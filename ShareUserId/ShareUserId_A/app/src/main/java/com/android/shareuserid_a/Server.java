e19085c580b705945d5769a77c38991d19e60da7
zhaozepeng <zhao_zepeng@hotmail.com>
2015-12-04 10:13:14 +0800

    项目名字变更

---
+++ ShareUserId/ShareUserId_A/app/src/main/java/com/android/shareuserid_a/Server.java
@@ -0,0 +1,10 @@
+package com.android.shareuserid_a;
+
+/**
+ * Description: 服务器端
+ *
+ * @author zzp(zhao_zepeng@hotmail.com)
+ * @since 2015-12-04
+ */
+public class Server {
+}
---
+++ ShareUserId/ShareUserId_B/app/src/main/java/com/android/shareuserid_b/Client.java
@@ -0,0 +1,10 @@
+package com.android.shareuserid_b;
+
+/**
+ * Description: 客户端
+ *
+ * @author zzp(zhao_zepeng@hotmail.com)
+ * @since 2015-12-04
+ */
+public class Client {
+}
